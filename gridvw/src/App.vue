c0681a71d86887c4ba2a5c2d667176f9bc87f08d
unknown <mihir1296@gmail.com>
2019-06-13 14:15:00 -0500

    Gridvw initialized

--- gridvw/src/App.vue
+++ gridvw/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WebPage />
+    <WebPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebPage from './components/WebPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WebPage
+  },
+  data() {
+    return {
+      url: "https://google.com"
+    }
   }
 }
 </script>
---
+++ gridvw/src/components/WebPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="web-page">
+    <div class="row">
+      <div class="input-field col s6">
+        <input v-model="height" id="height" type="text">
+        <label for="height">Component Height</label>
+      </div>
+      <div class="input-field col s6">
+        <input v-model="width" id="width" type="text">
+        <label for="width">Component Width</label>
+      </div>
+    </div>
+
+    <div class="row">
+      <input v-model="url" placeholder="url" id="url" type="text">
+        <label for="url">Component URL</label>
+    </div>
+    <!-- <input  type="text" name="Component Height"> -->
+    <iframe v-bind:src="url" frameborder="0" :height="height" :width="width"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebPage',
+  data() {
+    return {
+      url: "",
+      height: 0,
+      width: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
