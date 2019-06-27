85d2cb0b1649eb5e4c034e303d3d3962236cb5e9
unknown <mihir1296@gmail.com>
2019-06-27 15:15:18 -0500

    Server end Flask

---
+++ backend/server.py
@@ -0,0 +1,56 @@
+import datetime
+import json
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, JSON
+engine = create_engine('sqlite:///database.db', echo = True)
+meta = MetaData()
+
+views = Table(
+   'views', meta, 
+   Column('view_id', Integer, primary_key = True), 
+   Column('user_id', String, foreign_key = True),
+   Column('create_date', Date),
+   Column('view_config', JSON)
+)
+meta.create_all(engine)
+
+engine = create_engine('sqlite:///database.db')
+conn = engine.connect()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 200
+
+@app.route('/loads',methods=['GET'])
+def loads():
+
+    if request.method == 'GET':
+
+        view_id = request.args.get('view_id')
+
+        query = views.select().where(view_id=view_id)
+        result = conn.execute(query)
+
+        first_row = dict(result.fetchone())
+
+        return jsonify(first_row)
+
+@app.route('/save',methods=['POST','PATCH'])
+def save():
+
+    if request.method == 'POST':
+        view_data = request.json
+        user_id = view_data.get('user_id')
+        view_config = view_data.get('view_config')
+        create_date = datetime.datetime.now()
+
+        new = views.insert().values(user_id=user_id, create_date=create_date, view_config=json.dumps(view_config))
+        conn.execute(new)
+        
+        return jsonify({'msg':'Inserted view in table'})
+
+    
+if __name__=='__main__':
+    app.run()
