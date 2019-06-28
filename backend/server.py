57fa38717be8c91017d53e767ffcdb6d9f485a21
unknown <mihir1296@gmail.com>
2019-06-28 13:42:37 -0500

    Progress in back end CRUD

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,17 @@
 import datetime
 import json
+from uuid import uuid4
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, JSON
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, JSON, DateTime
 engine = create_engine('sqlite:///database.db', echo = True)
 meta = MetaData()
 
 views = Table(
    'views', meta, 
-   Column('view_id', Integer, primary_key = True), 
+   Column('id', String, primary_key = True), 
    Column('user_id', String),
-   Column('create_date', Date),
+   Column('create_date', DateTime),
+   Column('modify_date',DateTime),
    Column('view_config', JSON)
 )
 meta.create_all(engine)
@@ -18,40 +20,59 @@ engine = create_engine('sqlite:///database.db')
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 200
 
-@app.route('/load', methods=['GET'])
-def load():
-
+@app.route('/views', methods=['GET','POST','PATCH','DELETE'])
+def views_api():
+    
     if request.method == 'GET':
 
-        view_id = request.args.get('view_id')
+        view_id = request.args.get('id')
 
-        query = views.select().where(views.c.view_id==view_id)
+        query = views.select().where(views.c.id==view_id)
         conn = engine.connect()
         result = conn.execute(query)
 
         first_row = dict(result.fetchone())
 
         return jsonify(first_row)
-
-@app.route('/save', methods=['POST','PATCH'])
-def save():
+    
 
     if request.method == 'POST':
+        view_id = str(uuid4())
         view_data = request.json
         user_id = view_data.get('user_id')
-        view_config = view_data.get('view_config')
+        view_config = json.dumps(view_data.get('view_config'))
         create_date = datetime.datetime.now()
 
-        new = views.insert().values(user_id=user_id, create_date=create_date, view_config=json.dumps(view_config))
+        new = views.insert().values(id=view_id, user_id=user_id, create_date=create_date, modify_date=create_date, view_config=view_config)
         conn = engine.connect()
         conn.execute(new)
         
-        return jsonify({'msg':'Inserted view in table'})
+        return jsonify({'msg':f'View created for {view_id}'})
+
+    if request.method == 'PATCH':
+        view_data = request.json
+        view_id = view_data.get('view_id')
+        user_id = view_data.get('user_id')
+        view_config = json.dumps(view_data.get('view_config'))
+        modify_date = datetime.datetime.now()
+
+        update = views.update().where(views.c.id==view_id).values(modify_date=modify_date, view_config=view_config)
+
+        conn = engine.connect()
+        conn.execute(update)
+
+        return jsonify({'msg':f'Updated {view_id}'})
 
     
+    if request.method == 'DELETE':
+        view_id = request.args.get('id')
+
+        query = views.delete().where(views.c.id==view_id)
+        conn = engine.connect()
+        result = conn.execute(query)
+
+        return jsonify(dict(result))
+
 if __name__=='__main__':
     app.run()
